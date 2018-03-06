3070dafca619dd78774c61991efc3dbe15416abe
Gabriele Portente <gabriele.portente@gmail.com>
2018-03-07 00:49:52 +0100

    Part 4

---
+++ pkg/include/rg_version_data.h
@@ -0,0 +1,13 @@
+#define RG_VERSION "5.4.8.1.287.1.81.1.18.2.1"
+
+#define RG_VERSION_1 5
+#define RG_VERSION_2 4
+#define RG_VERSION_3 8
+#define RG_VERSION_4 1
+#define RG_VERSION_5 287
+#define RG_VERSION_6 1
+#define RG_VERSION_7 81
+#define RG_VERSION_8 1
+#define RG_VERSION_9 18
+#define RG_VERSION_10 2
+#define RG_VERSION_11 1
